dfb47fe60f0f240aa3038a691741ed27820a2166
bodnariuc <62306413+bodnariuc@users.noreply.github.com>
2020-11-18 16:54:08 +0100

    Update class_strings.php

--- class_strings.php
+++ class_strings.php
@@ -1,135 +1,248 @@
 <?php
 
-class StringsBodnariuc {
+declare(strict_types = 1);
+
+class StringsLukasBodnariuc{
     
-    public static function substrBodnariuc() {
-        $string = 'abcdef';
-        $start = -3;
-        return substr($string, $start);
-    }
-    
-    public static function substr_replaceBodnariuc() {
-        $string = 'abcdef';
-        $replacement = '666';
-        $start = 1;
-        $end = 3; 
-        return substr_replace($string, $replacement, $start, $end);
-    }
-    
-    public static function strtrBodnariuc() {
-        $string = 'ababba';
-        $trans = array("ab" => "01");
-        return strtr($string, $trans);
+    /**
+     * 
+     * @param string $retezec
+     * @param int $pozice
+     * @return type
+     */
+    public static function substrLukasBodnariuc(string $retezec, int $pozice) {
+        
+        return substr($retezec, $pozice);
     }
     
-    public static function strtoupperBodnariuc() {
-        $string = "ahoj";
-        return strtoupper($string);
+    /**
+     * 
+     * @param string $retezec
+     * @param string $retezec2
+     * @param int $zacatek
+     * @param type $konec
+     * @return string
+     */
+    public static function substr_replaceLukasBodnariuc(string $retezec, string $retezec2, int $zacatek, $konec = NULL): string {
+        
+        if (is_null($konec)) {
+            return substr_replace($retezec, $retezec2, $zacatek);
+        }
+        else {
+            return substr_replace($retezec, $retezec2, $zacatek, $konec); 
+        }
     }
     
-    public static function strtolowerBodnariuc() {
-        $string = "Ahoj";
-        return strtolower($string);
+    /**
+     * 
+     * @param string $retezec
+     * @param string $z_retezce
+     * @param string $do_retezce
+     * @return string
+     */
+    public static function strtrLukasBodnariuc(string $retezec, string $z_retezce, string $do_retezce): string {
+        
+        return strtr($retezec, $z_retezce, $do_retezce);
     }
     
-    public static function strrposBodnariuc() {
-        $string = "Ahoj";
-        $hledanaVec = "ho";
-        return strrpos($string, $hledanaVec);
+    /**
+     * 
+     * @param string $retezec
+     * @return string
+     */
+    public static function strtoupperLukasBodnariuc(string $retezec): string {
+        
+        return strtoupper($retezec);
     }
-
-    public static function strriposBodnariuc() {
-        $string = "Ahoj";
-        $hledanaVec = "ho";
-        return strripos($string, $hledanaVec);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @return string
+     */
+    public static function strtolowerLukasBodnariuc(string $retezec): string {
+        
+        return strtolower($retezec);
     }
-
-    public static function strposBodnariuc() {
-        $string = "Ahoj";
-        $hledanaVec = "h";
-        return strpos($string, $hledanaVec);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @param string $podretezec
+     * @param type $pozice
+     * @return int
+     */
+    public static function strrposLukasBodnariuc(string $retezec, string $podretezec, $pozice = NULL): int {
+        
+        if (is_null($pozice)){
+            return strrpos($retezec, $podretezec);
+        }
+        else {
+            return strrpos($retezec, $podretezec, $pozice);
+        }
     }
-
-    public static function strlenBodnariuc() {
-        $string = "Test";
-        return strlen($string);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @param string $podretezec
+     * @param type $pozice
+     * @return type
+     */
+    public static function strriposLukasBodnariuc(string $retezec, string $podretezec, $pozice = NULL) {
+        
+        if (is_null($pozice)){
+            return strripos($retezec, $podretezec);
+        }
+        else {
+            return strripos($retezec, $podretezec, $pozice);
+        }
     }
-
-    public static function strchrBodnariuc() {
-        $string = "Co ja vim";
-        $needle = "vim";
-        return strchr($string, $needle);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @param string $podretezec
+     * @param type $pozice
+     * @return type
+     */
+    public static function strposLukasBodnariuc(string $retezec, string $podretezec, $pozice = NULL) {
+        
+        if (is_null($pozice)){
+            return strpos($retezec, $podretezec);
+        }
+        else {
+            return strpos($retezec, $podretezec, $pozice);
+        }
     }
- 
-    public static function str_replaceBodnariuc() {
-        $search = array('A', 'B', 'C', 'D', 'E', 'F');
-        $replace = array('B', 'C', 'D', 'E', 'F', 'G');
-        $subject = 'A';
-        return str_replace($search, $replace, $subject);
+        
+    /**
+     * 
+     * @param string $retezec
+     * @return int
+     */
+    public static function strlenLukasBodnariuc(string $retezec): int {
+        
+        return strlen($retezec);
     }
- 
-    public static function md5Bodnariuc() {
-        $string = 12345;
-        return md5($string);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @param string $retezec2
+     * @param type $zarazka
+     * @return string
+     */
+    public static function strchrLukasBodnariuc(string $retezec, string $retezec2, $zarazka=FALSE): string {
+        
+        if ($zarazka){
+            return strchr($retezec, $retezec2, $zarazka);
+        }
+        else {
+            return strchr($retezec, $retezec2);
+        }
     }
-
-    public static function implodeBodnariuc() {
-        $glue = array('jmeno', 'prijmeni', 'cislo');
-        $doplneni = " , ";
-        return implode($glue, $doplneni);
+    
+    /**
+     * 
+     * @param string $vyhledavano
+     * @param string $nahrazovano
+     * @param string $retezec
+     * @param type $pocet
+     * @return string
+     */
+    public static function str_replaceLukasBodnariuc(string $vyhledavano, string $nahrazovano, string $retezec, $pocet=NULL): string {
+        
+        if (is_null($pocet)){
+            return str_replace($vyhledavano, $nahrazovano, $retezec);
+        }
+        else {
+            return str_replace($vyhledavano, $nahrazovano, $retezec, $pocet);
+        }
     }
-
-    public static function explodeBodnariuc() {
-        $delimiter = '""';
-        $string = "jmeno prijmeni cislo"; 
-        return explode($delimiter, $string);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @return string
+     */
+    public static function md5LukasBodnariuc(string $retezec): string {
+        
+        return md5($retezec);
     }
-
-    public static function addcslashesBodnariuc() {
-        $str = "Nevim co sem psat";
-        $charlist = 'A..z'; 
-        return addcslashes($str, $charlist);
+    
+    /**
+     * 
+     * @param string $separator
+     * @param array $pole
+     * @return string
+     */
+    public static function implodeLukasBodnariuc(string $separator, array $pole): string {
+        
+        return implode($separator, $pole);
     }
-
-    public static function addslashesBodnariuc() {
-        $string = "Co ja' vim"; 
-        return addslashes($string);
+    
+    /**
+     * 
+     * @param string $separator
+     * @param string $retezec
+     * @return array
+     */
+    public static function explodeLukasBodnariuc(string $separator, string $retezec): array {
+        
+        return explode($separator, $retezec);
     }
-
-    public static function trimBodnariuc() {
-        $string = "\atrimtest\a";
-        $string2 = "\a";
-        return trim($string, $string2);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @param string $char_list
+     * @return string
+     */
+    public static function addcslashesLukasBodnariuc(string $retezec, string $char_list): string {
+        
+        return addcslashes($retezec, $char_list);
     }
-
-    public static function ucfirstBodnariuc() {
-        $string = "ahoj";  
-        return ucfirst($string);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @return string
+     */
+    public static function addslashesLukasBodnariuc(string $retezec): string {
+        
+        return addslashes($retezec);
     }
-
-    public static function ucwordsBodnariuc() {
-        $string = "Co ja vim"; 
-        return ucwords($string);
+    
+    /**
+     * 
+     * @param string $retezec
+     * @return string
+     */
+    public static function trimLukasBodnariuc(string $retezec): string {
+        
+        return trim ($retezec);
+    }
+    
+    /**
+     * 
+     * @param string $retezec
+     * @return string
+     */
+    public static function ucfirstLukasBodnariuc(string $retezec): string {
+        
+        return ucfirst($retezec);
+    }
+    
+    /**
+     * 
+     * @param string $retezec
+     * @return string
+     */
+    public static function ucwordsLukasBodnariuc(string $retezec): string {
+        
+        return ucwords($retezec);
     }
 }
-var_dump(StringsBodnariuc::substrBodnariuc());
-var_dump(StringsBodnariuc::substr_replaceBodnariuc());
-var_dump(StringsBodnariuc::strtrBodnariuc());
-var_dump(StringsBodnariuc::strtoupperBodnariuc());
-var_dump(StringsBodnariuc::strtolowerBodnariuc());
-var_dump(StringsBodnariuc::strrposBodnariuc());
-var_dump(StringsBodnariuc::strriposBodnariuc());
-var_dump(StringsBodnariuc::strposBodnariuc());
-var_dump(StringsBodnariuc::strlenBodnariuc());
-var_dump(StringsBodnariuc::strchrBodnariuc());
-var_dump(StringsBodnariuc::str_replaceBodnariuc());
-var_dump(StringsBodnariuc::md5Bodnariuc());
-var_dump(StringsBodnariuc::implodeBodnariuc());
-var_dump(StringsBodnariuc::explodeBodnariuc());
-var_dump(StringsBodnariuc::addcslashesBodnariuc());
-var_dump(StringsBodnariuc::addslashesBodnariuc());
-var_dump(StringsBodnariuc::trimBodnariuc());
-var_dump(StringsBodnariuc::ucfirstBodnariuc());
-var_dump(StringsBodnariuc::ucwordsBodnariuc());
-?>
 
+?>
